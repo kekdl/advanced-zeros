43943bf88dcfb3fa6e8718c7ad26920f09df9c5a
kekdl <36787202+kekdl@users.noreply.github.com>
2018-03-06 23:18:51 +0300

    Update and rename src/index.js to  src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,89 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    // your implementation
+
+    function simple(num) {
+        var x = num;
+        var sqrt = Math.sqrt(x);
+        var currentValue = x;
+        var multiplier = 2;
+        var result = [];
+        while (currentValue > 1 && multiplier <= sqrt) {
+            if (currentValue % multiplier == 0) {
+                result.push(multiplier);
+                currentValue /= multiplier;
+            } else if (multiplier == 2) {
+                multiplier++;
+            } else {
+                multiplier += 2;
+            }
+        }
+        if (currentValue != 1) {
+            result.push(currentValue);
+        }
+        return result;
+
+    }
+
+    // base = 72;
+    var result = simple(base);
+    //  number = 1000;
+    console.log(result);
+    var a = [];
+    var resultlength = result.length;
+
+    var p = new Set();
+    for (var i = 0; i < resultlength; i++) {
+        p.add(result[i])
+    }
+    console.log(p);
+
+    var power = 0;
+    var temp = result[0];
+
+    for (var value of p.values()) {
+        for (var i = 0; i < resultlength; i++) {
+            if (value == result[i]) power++;
+        }
+        a.push(power);
+        power = 0;
+    }
+
+
+    console.log(a, p);
+    var sa = [];
+    var pp = [];
+    var i = 0;
+    for (var value of p.values()) {
+        sa[i] = 0;
+        pp[i] = value;
+        i++;
+
+    }
+    console.log(sa, pp);
+
+    for (var i = 2; i <= number; i++) {
+        tmp = i;
+        var j = 0;
+        for (var value of p.values()) { //цикл по всем простым числам, составляющим основание, в примере L должно быть равно 2
+            while (!(tmp % value)) {
+                tmp = tmp / value;
+                sa[j]++;
+            } //считаем кол-во степеней
+            j++;
+        }
+    }
+    console.log(sa);
+
+    tmp = Math.floor(sa[0] / a[0]);
+    console.log(tmp);
+
+    for (var i = 0; i < pp.length; i++) {
+        if (tmp < Math.floor(sa[i] / pp[i]))
+            tmp = Math.floor(sa[i] / pp[i]);
+        console.log(i, sa[i], pp[i], tmp);
+
+    }
+    console.log(tmp);
+    return tmp;
+
 }
